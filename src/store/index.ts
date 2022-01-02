0cadaf7adaa5826b50f84060f7233a842cd8fa68
kverenev <kverenev@gmail.com>
2022-01-02 20:48:32 +0300

    reactotron setup and configure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
-import { applyMiddleware, createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import Reactotron from '../../reactotronConfig';
 
 import rootReducer from './combineReducer';
 
-export const Store = createStore(rootReducer, applyMiddleware(thunk));
+export const Store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk), Reactotron.createEnhancer!())
+);
