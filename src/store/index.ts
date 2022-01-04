66036edccb369a8f2fe36b3504dc03d59648e9a0
kverenev <kverenev@gmail.com>
2022-01-04 21:38:10 +0300

    add __dev__ variable to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,30 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, Store as IStore } from 'redux';
 import thunk from 'redux-thunk';
+
 import Reactotron from '../../reactotronConfig';
+import { AlbumsStateAction } from '../features/albums/actions';
+import { ArtistStateAction } from '../features/artists/actions';
+import { mainStateAction } from './actions';
+import rootReducer, { RootReducer } from './combineReducer';
 
-import rootReducer from './combineReducer';
+let Store: IStore<RootReducer, mainStateAction | ArtistStateAction | AlbumsStateAction>;
 
-export const Store = createStore(
-  rootReducer,
-  compose(applyMiddleware(thunk), Reactotron.createEnhancer!())
-);
+if (!__DEV__) {
+  Store = createStore(rootReducer, applyMiddleware(thunk));
+} else {
+  Store = createStore(rootReducer, compose(applyMiddleware(thunk), Reactotron.createEnhancer!()));
+}
+
+export { Store };
+
+// another variant
+
+//let enhancer: StoreEnhancer;
+
+//if (!__DEV__) {
+//  enhancer = applyMiddleware(thunk);
+//} else {
+//  enhancer = compose(applyMiddleware(thunk), Reactotron.createEnhancer!());
+//}
+
+//export const Store = createStore(rootReducer, enhancer);
