e5b4dccda36ebbbdfb8a100b8775ddba15c089a8
kverenev <kverenev@gmail.com>
2022-01-05 13:04:52 +0300

    rename Store to store and remove comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose, Store as IStore } from 'redux';
+import { createStore, applyMiddleware, compose, Store } from 'redux';
 import thunk from 'redux-thunk';
 
 import Reactotron from '../../reactotronConfig';
@@ -7,24 +7,12 @@ import { ArtistStateAction } from '../features/artists/actions';
 import { mainStateAction } from './actions';
 import rootReducer, { RootReducer } from './combineReducer';
 
-let Store: IStore<RootReducer, mainStateAction | ArtistStateAction | AlbumsStateAction>;
+let store: Store<RootReducer, mainStateAction | ArtistStateAction | AlbumsStateAction>;
 
 if (!__DEV__) {
-  Store = createStore(rootReducer, applyMiddleware(thunk));
+  store = createStore(rootReducer, applyMiddleware(thunk));
 } else {
-  Store = createStore(rootReducer, compose(applyMiddleware(thunk), Reactotron.createEnhancer!()));
+  store = createStore(rootReducer, compose(applyMiddleware(thunk), Reactotron.createEnhancer!()));
 }
 
-export { Store };
-
-// another variant
-
-//let enhancer: StoreEnhancer;
-
-//if (!__DEV__) {
-//  enhancer = applyMiddleware(thunk);
-//} else {
-//  enhancer = compose(applyMiddleware(thunk), Reactotron.createEnhancer!());
-//}
-
-//export const Store = createStore(rootReducer, enhancer);
+export { store };
