7d517217122fe6399459badc8e37a87ebddc388e
kverenev <kverenev@gmail.com>
2022-01-11 15:45:02 +0300

    add a type of iTunes response

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,32 +9,46 @@ import {
   TrackResponseData as TrackResponseRecord,
 } from '../features/albums/types';
 
+type iTunesResponse<Items> = {
+  results: Items;
+};
+
 const instance = axios.create({
   baseURL: 'https://itunes.apple.com',
 });
 
 export const artistAPI = {
   async getArtistsByName(name: string): Promise<ArtistResponceData[]> {
-    const response = await instance.get(`/search?term=${name}&entity=musicArtist`);
+    const response = await instance.get<iTunesResponse<ArtistResponceData[]>>(
+      `/search?term=${name}&entity=musicArtist`
+    );
     return response.data.results;
   },
   async getArtistAlbumsById(id: number): Promise<AlbumsResponseData[]> {
-    const response = await instance.get(`/lookup?id=${id}&entity=album`);
+    const response = await instance.get<iTunesResponse<AlbumsResponseData[]>>(
+      `/lookup?id=${id}&entity=album`
+    );
     return response.data.results.slice(1);
   },
   async getTracksById(id: number): Promise<TrackResponseData[]> {
-    const response = await instance.get(`/lookup?id=${id}&entity=song`);
+    const response = await instance.get<iTunesResponse<TrackResponseData[]>>(
+      `/lookup?id=${id}&entity=song`
+    );
     return response.data.results.slice(1);
   },
 };
 
 export const albumAPI = {
   async getAlbumsByName(name: string): Promise<AlbumsResponseRecord[]> {
-    const response = await instance.get(`/search?term=${name}&attribute=albumTerm&entity=album`);
+    const response = await instance.get<iTunesResponse<AlbumsResponseRecord[]>>(
+      `/search?term=${name}&attribute=albumTerm&entity=album`
+    );
     return response.data.results;
   },
   async getTracksById(id: number): Promise<TrackResponseRecord[]> {
-    const response = await instance.get(`/lookup?id=${id}&entity=song`);
+    const response = await instance.get<iTunesResponse<TrackResponseRecord[]>>(
+      `/lookup?id=${id}&entity=song`
+    );
     return response.data.results.slice(1);
   },
 };
