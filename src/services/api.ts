6f780e25a233af782093071a32abbe3f7863c1f5
kverenev <kverenev@gmail.com>
2022-02-02 14:43:01 +0300

    get country with localize

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import * as RNLocalize from 'react-native-localize';
+
 import {
   AlbumsResponseData,
   ArtistResponceData,
@@ -18,22 +20,26 @@ const instance = axios.create({
   baseURL: 'https://itunes.apple.com',
 });
 
+const country = RNLocalize.getCountry() ? RNLocalize.getCountry() : 'US';
+
+console.log(`current country:  ${country}`);
+
 export const artistAPI = {
   async getArtistsByName(name: string): Promise<ArtistResponceData[]> {
     const response = await instance.get<iTunesResponse<ArtistResponceData[]>>(
-      `/search?term=${name}&entity=musicArtist`
+      `/search?term=${name}&entity=musicArtist&country=${country}`
     );
     return response.data.results;
   },
   async getArtistAlbumsById(id: number): Promise<AlbumsResponseData[]> {
     const response = await instance.get<iTunesResponse<AlbumsResponseData[]>>(
-      `/lookup?id=${id}&entity=album`
+      `/lookup?id=${id}&entity=album&country=${country}`
     );
     return response.data.results.slice(1);
   },
   async getTracksById(id: number): Promise<TrackResponseData[]> {
     const response = await instance.get<iTunesResponse<TrackResponseData[]>>(
-      `/lookup?id=${id}&entity=song`
+      `/lookup?id=${id}&entity=song&country=${country}`
     );
     return response.data.results.slice(1);
   },
@@ -42,13 +48,13 @@ export const artistAPI = {
 export const albumAPI = {
   async getAlbumsByName(name: string): Promise<AlbumsResponseRecord[]> {
     const response = await instance.get<iTunesResponse<AlbumsResponseRecord[]>>(
-      `/search?term=${name}&attribute=albumTerm&entity=album`
+      `/search?term=${name}&attribute=albumTerm&entity=album&country=${country}`
     );
     return response.data.results;
   },
   async getTracksById(id: number): Promise<TrackResponseRecord[]> {
     const response = await instance.get<iTunesResponse<TrackResponseRecord[]>>(
-      `/lookup?id=${id}&entity=song`
+      `/lookup?id=${id}&entity=song&country=${country}`
     );
     return response.data.results.slice(1);
   },
@@ -57,7 +63,7 @@ export const albumAPI = {
 export const favoritesAPI = {
   async getAlbumsByIds(ids: number[]): Promise<FavoriteAlbumsData[]> {
     const response = await instance.get<iTunesResponse<FavoriteAlbumsData[]>>(
-      `/lookup?id=${ids.join()}&entity=album`
+      `/lookup?id=${ids.join()}&entity=album&country=${country}`
     );
     return response.data.results;
   },
